e0a109e3ce8cef8fd0898ddb529f4aeb332e34c8
jkhu365 <junkai.hu@redteamobile.com>
2023-12-11 11:50:33 +0800

    修改脚本文件

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jkhu365/docs.git main:deploy
+git push -f git@github.com:jkhu365/docs.git master
 
 cd -
